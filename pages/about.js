ba8dffd78e3f97ded3bfedb5d7782b3894af158c
ito <ito@argyle.jp>
2022-11-23 14:58:09 +0900

    修正

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { Footer } from "../components/Footer";
 
 import styles from "../styles/Home.module.css";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <title>About Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
       <Main page="about" />
       <Footer />
